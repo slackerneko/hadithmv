c3e24e81cd22f2445b374160b465a76348328cbd
slackerneko <slackerneko@gmail.com>
2025-11-25 16:51:32 +0500

    removed deno dependency

---
+++ serviceworker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+   console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+   console.log("Service worker activated");
+});
+
+self.addEventListener("fetch", event => {
+  console.log("Service worker fetch")
+});
+
+
+caches.open("assets")
+  .then(cache => {
+    cache.addAll([
+      "styles/main.css",
+      "styles/colors.css",
+      "styles/typography.css"
+    ])
+  })
